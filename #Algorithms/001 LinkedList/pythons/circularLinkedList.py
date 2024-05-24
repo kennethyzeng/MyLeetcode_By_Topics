67e73cb78e46d6288764f9f710bcfd7c1bb0d131
Kenneth Zeng <kennethyzeng@gmail.com>
2024-05-23 17:57:01 -0700

    add

--- #Algorithms/001 LinkedList/pythons/circularLinkedList.py
+++ #Algorithms/001 LinkedList/pythons/circularLinkedList.py
@@ -1,3 +1,10 @@
+"""
+Circular linked list: 
+    -tail.next = head 
+    -need self.head and self.point 
+    -two method: append(), and delete()
+    -for the delete(), need one while loop with two pointer: prev and current
+"""
 class Node: 
     """A Circular linked Node"""
     def __init__(self, data=None):
@@ -24,15 +31,69 @@ class CircularLinkedList:
     
     #need two extra pter: current and prev
     def delete(self, data):
-        current=self.head 
-        prev=self.head 
+        current=self.head #pt 1
+        prev=self.head    #pt 2
         flag = False #to determinte if the node exist 
-        while prev==current or current==self.tail:
+        while prev==current or prev!=self.tail:  #iteration
             if current.data == data:
                 #condition 1: item to be deleted is head node  
                 if current == self.head: 
-                    
+                    self.head=current.next 
+                    self.tail.next =self.head 
+                #contition 2: item to be deleted is tail node
+                elif current == self.tail:
+                    self.tail = prev 
+                    prev.next=self.head 
+                #condition 3: item to be deleted is intermdeiate node 
+                else: 
+                    prev.next =current.next 
+                self.size -=1 
+                return   #not correct if miss this one
+            prev = current 
+            current = current.next 
+        if flag is False: 
+            print("Item not present in the list")
 
 
-    def iter():
-        pass 
+    def iter(self):
+        current = self.head 
+        while current:
+            val =current.data 
+            current=current.next 
+            yield val 
+
+#case validation
+words =CircularLinkedList()
+words.append("ham")
+words.append("Spam")
+words.append("egg")
+
+
+"""
+counter=0 
+for word in words.iter():
+    print(word)
+    counter += 1 
+    if counter >2:
+        break 
+print("*" * 16)
+"""
+words.append('foo')
+words.append('bar')
+words.append('bim')
+words.append('baz')
+words.append('quux')
+words.append('duux')
+
+print('Let us delete something that is there.')
+#words.delete('ham')
+#words.delete('bar')
+words.delete('duux')
+#words.delete('socks')
+print('back to iterating')
+counter = 0
+for item in words.iter():
+    print(item)
+    counter += 1
+    if counter == 9:
+        break
